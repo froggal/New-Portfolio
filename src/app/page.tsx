be280ba3c77affdcc884b1dcd74b1b7fe303878d
keyfrog@outlook.kr <keyfrog@outlook.kr>
2023-09-17 20:16:32 +0900

    bootstrap icon으로 교체

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,16 +4,21 @@ export default function Home() {
   return (
     <>
       <div className="bg-green-400 box-border h-100 w-100 p-4 border-4 mt-5 text-center">
-      <h1 className="mt-10 text-blue-700 font-extrabold text-9xl">키페</h1>
-      <h2 className="my-10 text-4xl font-bold">김도언, KeyFrog</h2>
-      <h3 className="my-10 text-3xl font-semibold">학생 개발자</h3>
-    </div>
-    <div className="text-center bg-orange-200 box-border h-100 w-100 p-4 border-4" id="projects">
-      <h1 className="mt-10 font-extrabold text-5xl">프로젝트들</h1>
-      <Link href="/projects"><button className="my-10 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-      보러 가기!
-      </button></Link>
-    </div>
+        <h1 className="mt-10 text-blue-700 font-extrabold text-9xl">키페</h1>
+        <h2 className="my-10 text-4xl font-bold">김도언, KeyFrog</h2>
+        <h3 className="my-10 text-3xl font-semibold">학생 개발자</h3>
+      </div>
+      <div
+        className="text-center bg-orange-200 box-border h-100 w-100 p-4 border-4"
+        id="projects"
+      >
+        <h1 className="mt-10 font-extrabold text-5xl">프로젝트들</h1>
+        <Link href="/projects">
+          <button className="my-10 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+            보러 가기!
+          </button>
+        </Link>
+      </div>
     </>
   );
 }
--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -13,22 +13,73 @@ export default function projects() {
         className="text-center bg-orange-200 box-border h-100 w-100 p-4 border-4"
         id="projects"
       >
-          <h1 className="mt-10 font-extrabold text-3xl">포트폴리오</h1>
-          <h2 className="mt-5">지금 보고 계시는 이 웹페이지입니다. 이 프로젝트를 진행하면서 Next.js를 접하게 되었습니다. <br /> Next.js와 TypeScript를 이용해 개발했고, Vercel로 배포했습니다.</h2>
-          <a href="https://kpj-portfolio.vercel.app/"><button className="my-10 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-            보러 가기!
-          </button></a>
-          <a href="https://github.com/froggal/New-Portfolio"><button className="my-10 ml-5 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-            GitHub에서 보기!
-          </button></a>
-          <h1 className="mt-10 font-extrabold text-3xl">랜덤 닉네임 생성기</h1>
-          <h2 className="mt-5">랜덤으로 닉네임을 생성해주는 웹 서비스입니다. <br /> HTML과 JavaScript로 개발했고, Vercel로 배포했습니다.</h2>
-          <a href="https://randomnick.vercel.app"><button className="my-10 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-            보러 가기!
-          </button></a>
-          <a href="https://github.com/froggal/Random-Nickname-Generator"><button className="my-10 ml-5 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-            GitHub에서 보기!
-          </button></a>
+        <h1 className="mt-10 font-extrabold text-3xl">포트폴리오</h1>
+        <h2 className="mt-5">
+          지금 보고 계시는 이 웹페이지입니다. 이 프로젝트를 진행하면서 Next.js를
+          접하게 되었습니다. <br /> Next.js와 TypeScript를 이용해 개발했고,
+          Vercel로 배포했습니다.
+        </h2>
+        <a href="https://kpj-portfolio.vercel.app/">
+          <button className="my-10 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="16"
+              height="16"
+              fill="currentColor"
+              className="bi bi-play-fill"
+              viewBox="0 0 16 16"
+            >
+              <path d="m11.596 8.697-6.363 3.692c-.54.313-1.233-.066-1.233-.697V4.308c0-.63.692-1.01 1.233-.696l6.363 3.692a.802.802 0 0 1 0 1.393z" />
+            </svg>
+          </button>
+        </a>
+        <a href="https://github.com/froggal/New-Portfolio">
+          <button className="my-10 ml-5 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="16"
+              height="16"
+              fill="currentColor"
+              className="bi bi-github"
+              viewBox="0 0 16 16"
+            >
+              <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.012 8.012 0 0 0 16 8c0-4.42-3.58-8-8-8z" />
+            </svg>
+          </button>
+        </a>
+        <h1 className="mt-10 font-extrabold text-3xl">랜덤 닉네임 생성기</h1>
+        <h2 className="mt-5">
+          랜덤으로 닉네임을 생성해주는 웹 서비스입니다. <br /> HTML과
+          JavaScript로 개발했고, Vercel로 배포했습니다.
+        </h2>
+        <a href="https://randomnick.vercel.app">
+          <button className="my-10 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="16"
+              height="16"
+              fill="currentColor"
+              className="bi bi-play-fill"
+              viewBox="0 0 16 16"
+            >
+              <path d="m11.596 8.697-6.363 3.692c-.54.313-1.233-.066-1.233-.697V4.308c0-.63.692-1.01 1.233-.696l6.363 3.692a.802.802 0 0 1 0 1.393z" />
+            </svg>
+          </button>
+        </a>
+        <a href="https://github.com/froggal/Random-Nickname-Generator">
+          <button className="my-10 ml-5 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="16"
+              height="16"
+              fill="currentColor"
+              className="bi bi-github"
+              viewBox="0 0 16 16"
+            >
+              <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.012 8.012 0 0 0 16 8c0-4.42-3.58-8-8-8z" />
+            </svg>
+          </button>
+        </a>
       </div>
     </>
   );
