802d25745c3f94c4d517f556aa921176c00e176a
keyfrog@outlook.kr <keyfrog@outlook.kr>
2023-09-17 19:19:01 +0900

    프로젝트 페이지의 헤더 생성

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -3,6 +3,10 @@ import Link from "next/link";
 export default function projects() {
   return (
     <>
+        <div className="bg-green-400 box-border h-100 w-100 p-4 border-4 mt-5 text-center">
+            <h1 className="mt-10 text-blue-700 font-extrabold text-9xl">프로젝트</h1>
+            <h2 className="my-10 text-4xl font-bold">제가 지금까지 진행한 프로젝트들입니다.</h2>
+        </div>
     </>
   );
 }
