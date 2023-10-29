b5b6db95309baea46c7bd6965f95f8318768e860
keyfrog@outlook.kr <keyfrog@outlook.kr>
2023-10-29 17:49:44 +0900

    새로시작

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,60 +21,7 @@ export default function RootLayout({
         href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css"
       ></link>
       <body className={inter.className}>
-        <nav className="bg-white border-gray-200 dark:bg-gray-900 mt-5">
-          <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-            <a href="#" className="flex items-center">
-              <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
-                키페_KeyFrog
-              </span>
-            </a>
-            <button
-              data-collapse-toggle="navbar-default"
-              type="button"
-              className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
-              aria-controls="navbar-default"
-              aria-expanded="false"
-            >
-              <span className="sr-only">Open main menu</span>
-              <svg
-                className="w-5 h-5"
-                aria-hidden="true"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 17 14"
-              >
-                <path
-                  stroke="currentColor"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  d="M1 1h15M1 7h15M1 13h15"
-                />
-              </svg>
-            </button>
-            <div
-              className="hidden w-full md:block md:w-auto"
-              id="navbar-default"
-            >
-              <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700"></ul>
-            </div>
-          </div>
-        </nav>
         {children}
-        <div className="bg-gray-400 box-border h-100 w-100 p-4 border-4 text-center">
-          <p className="font-semibold my-5">
-            Copyright 2023 김 도언, All right reserved
-          </p>
-          <a href="mailto:kpjdev.21k@gmail.com" className="mr-5">
-            <i className="bi bi-envelope-fill"></i>
-          </a>
-          <a href="https://github.com/froggal" className="mr-5">
-            <i className="bi bi-github"></i>
-          </a>
-          <a href="https://keyfrog.tistory.com">
-            <i className="bi bi-book"></i>
-          </a>
-        </div>
       </body>
     </html>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,35 +3,7 @@ import Link from "next/link";
 export default function Home() {
   return (
     <>
-      <div className="bg-green-400 box-border h-100 w-100 p-4 border-4 mt-5 text-center">
-        <h1 className="mt-10 text-blue-700 font-extrabold text-9xl">키페</h1>
-        <h2 className="my-10 text-4xl font-bold">김도언, KeyFrog</h2>
-        <h3 className="my-10 text-3xl font-semibold">학생 개발자</h3>
-      </div>
-      <div
-        className="text-center bg-orange-200 box-border h-100 w-100 p-4 border-4"
-        id="projects"
-      >
-        <h1 className="mt-10 font-extrabold text-5xl">프로젝트들</h1>
-        <Link href="/projects">
-          <button className="my-10 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="16"
-              height="16"
-              fill="currentColor"
-              className="bi bi-play-fill"
-              viewBox="0 0 16 16"
-            >
-              <path d="m11.596 8.697-6.363 3.692c-.54.313-1.233-.066-1.233-.697V4.308c0-.63.692-1.01 1.233-.696l6.363 3.692a.802.802 0 0 1 0 1.393z" />
-            </svg>
-          </button>
-        </Link>
-      </div>
-      <div className="bg-indigo-600 box-border h-100 w-100 p-4 border-4 text-center">
-        <h1 className="mt-10 font-extrabold text-5xl">기술 스택</h1>
-        <div className="rounded-full bg-blue-600">TypeScript</div>
-      </div>
+      <h1></h1>
     </>
   );
 }
