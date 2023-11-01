0e2c843c299332a6de042d1bd7d1615bc42086f8
keyfrog@outlook.kr <keyfrog@outlook.kr>
2023-11-01 15:28:02 +0900

    heading 생성

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
-import Image from 'next/image'
 import styles from './page.module.css'
 
 export default function Home() {
   return (
     <>
+      <div className={styles.heading}>
+        <h1>Developer: <br /> <span>KeyFrog</span>;</h1>
+        <h2>학생 개발자 KeyFrog(키페)입니다.</h2>
+      </div>
     </>
   )
 }
