a1cfde403cad7f7d21779b2679f6806aca2c2665
keyfrog@outlook.kr <keyfrog@outlook.kr>
2023-11-04 16:20:35 +0900

    소개에 약간의 농담 추가

--- app/page.tsx
+++ app/page.tsx
@@ -24,7 +24,7 @@ export default function Home() {
         <h1>안녕하세요, 키페입니다 👋</h1>
         <p>
           대한민국에 거주하고 있고 웹 개발을 좋아하는 학생이예요! <br /> 주로
-          사용하는 언어는 TypeScript/JavaScript, Python입니다. <br /> 최근에는 디자인을 배우고 있고, 서버
+          사용하는 언어는 TypeScript/JavaScript, Python입니다. <s>그리고 HTML로 프로그래밍하는 것을 좋아합니다 ㅋㅋ</s> <br /> 최근에는 디자인을 배우고 있고, 서버
           개발에도 관심이 많아서 현재 홈서버를 구동하고 있어요! <br /> Apple
           팬입니다! 개발은 모두 맥북으로 하고 있고 에어팟, 아이패드를 사용하고
           있어요.
