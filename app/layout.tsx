922bb4132b8d766e913a6651b19e88f781ceab03
keyfrog@outlook.kr <keyfrog@outlook.kr>
2023-11-08 16:22:40 +0900

    footer 완성

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,9 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
 import styles from './layout.module.css'
+import gstyles from './global.module.css'
+import footer from './footer.module.css'
+import Link from 'next/link'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -16,9 +19,14 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
+    <html lang="en" className={gstyles.main}>
       <body className={inter.className}>{children}</body>
-      
+      <div className={footer.main}>
+        <div>
+          <h1 className={footer.name}>KeyFrog</h1> <p>Copyright 2023. KeyFrog All rights reserved</p>
+          <p><Link href='https://litt.ly/keyfrog'>Links</Link></p>
+        </div>
+      </div>
     </html>
   )
 }
